6291da909a678fbee513a86dca9b8f82221117d4
Yukiji Nakatsuka <lunarbase@outlook.com>
2018-06-13 20:31:50 -0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+// Exercise of Prompt and Alert
+
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var month = prompt("Which month were you born?");
+var day = prompt("Which day were you born?");
+var year = prompt("Which year were you born?");
+
+alert("Hello, " + firstName + " " + lastName + "!" + " " + "Your were born: " + month + "/" + day + "/" + year + "!");
+
+
+// document.write("Hello, " + firstName + " " + lastName + " " + "!");
